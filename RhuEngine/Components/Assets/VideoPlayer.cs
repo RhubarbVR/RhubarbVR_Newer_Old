bb73fa587851332bf9cc72eca947608d7144342b
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2022-02-01 08:33:33 -0500

    Update VideoPlayer.cs

--- RhuEngine/Components/Assets/VideoPlayer.cs
+++ RhuEngine/Components/Assets/VideoPlayer.cs
@@ -116,18 +116,87 @@ namespace RhuEngine.Components
 			}
 		}
 
+		private enum Chroma
+		{
+			Uknown,
+			I420,
+			I411,
+			I422,
+			YUYV,
+			UYVY,
+			RV24,
+		    RV32,
+			I42N,
+			I41N,
+			GRAW,
+		}
+
+		private Chroma _loadedChroma = Chroma.Uknown;
+
+		private uint _pitches;
+
+		private uint _lines;
 		uint LibVLCVideoFormatCb(ref IntPtr opaque, IntPtr chroma, ref uint width, ref uint height, ref uint pitches, ref uint lines) {
-			var e = new Tex(TexType.ImageNomips);;
+			Log.Info($"Format:{Marshal.PtrToStringAnsi(chroma)} width{width} height{height} pitches{pitches}  lines{lines}");
+			_pitches = pitches;
+			_lines = lines;
+			_loadedChroma = Marshal.PtrToStringAnsi(chroma) switch {
+				"I420" => Chroma.I420,
+				"I411" => Chroma.I411,
+				"I422" => Chroma.I422,
+				"YUYV" => Chroma.YUYV,
+				"UYVY" => Chroma.UYVY,
+				"RV24" => Chroma.RV24,
+				"RV32" => Chroma.RV32,
+				"I42N" => Chroma.I42N,
+				"I41N" => Chroma.I41N,
+				"GRAW" => Chroma.GRAW,
+				_ => Chroma.Uknown,
+			};
+			var format = TexFormat.Rgba32;
+			if (_loadedChroma == Chroma.Uknown) {
+				return 1;
+			}
+			var e = new Tex(TexType.ImageNomips, format);
 			e.SetSize((int)width, (int)height);
 			Load(e);
-			//don't know how to force to rgb
-			Log.Info($"Format:{Marshal.PtrToStringAnsi(chroma)} width{width} height{height}");
 			return 1;
+		}
+
+		private void RenderI420(IntPtr data) {
+
 		}
 
 		void LibVLCVideoUnlockCb(IntPtr opaque, IntPtr picture, IntPtr planes) {
 			if(Value is null) {
 				return;
+			}
+			switch (_loadedChroma) {
+				case Chroma.Uknown:
+					break;
+				case Chroma.I420:
+					RenderI420(planes);
+					break;
+				case Chroma.I411:
+					break;
+				case Chroma.I422:
+					break;
+				case Chroma.YUYV:
+					break;
+				case Chroma.UYVY:
+					break;
+				case Chroma.RV24:
+					break;
+				case Chroma.RV32:
+					break;
+				case Chroma.I42N:
+					break;
+				case Chroma.I41N:
+					break;
+				case Chroma.GRAW:
+					break;
+				default:
+					break;
 			}
 			//need to load colors
 			//Value.SetColors(Value.Width, Value.Height, planes);
