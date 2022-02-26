d561cc712cbda51b9aa87dd0d348563b3e4c6870
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2022-02-25 19:45:40 -0500

    Update UserRoot.cs

--- RhuEngine/Components/User/UserRoot.cs
+++ RhuEngine/Components/User/UserRoot.cs
@@ -43,7 +43,7 @@ namespace RhuEngine.Components
 					Entity.GlobalTrans = focusUserRoot.Entity.GlobalTrans;
 				}
 				
-				Renderer.CameraRoot = ((SK.ActiveDisplayMode == DisplayMode.Flatscreen & SK.Settings.disableFlatscreenMRSim) ? head.Target?.GlobalTrans ?? Matrix.S(1) : Entity.GlobalTrans) * (StereoKit.World.HasBounds? StereoKit.World.BoundsPose.ToMatrix(): Matrix.Identity);
+				Renderer.CameraRoot = ((SK.ActiveDisplayMode == DisplayMode.Flatscreen & SK.Settings.disableFlatscreenMRSim) ? head.Target?.GlobalTrans ?? Matrix.S(1) : Entity.GlobalTrans) * Matrix.T(new Vec3(0, StereoKit.World.BoundsSize.y, 0));
 			}
 			else {
 				user.Target.FindOrCreateSyncStream<SyncValueStream<Vec3>>("UserPos").Value = Entity.position.Value;
