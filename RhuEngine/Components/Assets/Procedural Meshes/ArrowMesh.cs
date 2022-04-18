a92fac82644098fbb1644dc9c9490174f13a5043
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-04-17 19:55:40 -0500

    Create ArrowMesh.cs

---
+++ RhuEngine/Components/Assets/Procedural Meshes/ArrowMesh.cs
@@ -0,0 +1,59 @@
+﻿using RhuEngine.WorldObjects;
+using RhuEngine.WorldObjects.ECS;
+using RNumerics;
+
+namespace RhuEngine.Components.Assets.Procedural_Meshes
+{
+	[Category(new string[] { "Assets/Procedural Meshes" })]
+	public class ArrowMesh : ProceduralMesh
+	{
+		[Default(0.5f)]
+		[OnChanged(nameof(LoadMesh))]
+		public Sync<float> StickRadius;
+
+		[Default(1.0f)]
+		[OnChanged(nameof(LoadMesh))]
+		public Sync<float> StickLength;
+
+		[Default(1.0f)]
+		[OnChanged(nameof(LoadMesh))]
+		public Sync<float> HeadBaseRadius;
+
+		[Default(0.0f)]
+		[OnChanged(nameof(LoadMesh))]
+		public Sync<float> TipRadius;
+
+		[Default(0.5f)]
+		[OnChanged(nameof(LoadMesh))]
+		public Sync<float> HeadLength;
+
+		[Default(false)]
+		[OnChanged(nameof(LoadMesh))]
+		public Sync<bool> DoubleSided;
+
+		public override void FirstCreation() {
+			base.FirstCreation();
+		}
+
+		private void LoadMesh() {
+			if (!Engine.EngineLink.CanRender) {
+				return;
+			}
+			var mesh = new Radial3DArrowGenerator {
+				StickRadius = StickRadius,
+				StickLength = StickLength,
+				HeadBaseRadius = HeadBaseRadius,
+				TipRadius = TipRadius,
+				HeadLength = HeadLength,
+				DoubleSided = DoubleSided,
+			};
+			mesh.Generate();
+			GenMesh(mesh.MakeSimpleMesh());
+		}
+
+		public override void OnLoaded() {
+			base.OnLoaded();
+			LoadMesh();
+		}
+	}
+}
