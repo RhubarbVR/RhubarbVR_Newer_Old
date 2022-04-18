f4675e1c6ed55cd9f51ebc42992ce00ca375ef7a
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2022-04-18 18:43:35 -0400

    Clipboard changes

---
+++ RhuEngine/ClipBoard/ClipboardService_SetStandard.cs
@@ -0,0 +1,29 @@
+﻿#if (NETSTANDARD || NETFRAMEWORK || NET5_0_OR_GREATER)
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TextCopy;
+
+public static partial class ClipboardService
+{
+    static Func<string, CancellationToken, Task> CreateAsyncSet()
+    {
+		return RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+			? WindowsClipboard.SetTextAsync
+			: RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+			? OsxClipboard.SetTextAsync
+			: RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? LinuxClipboard.SetTextAsync : ((_, _) => throw new NotSupportedException());
+	}
+
+	static Action<string> CreateSet()
+    {
+		return RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+			? WindowsClipboard.SetText
+			: RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+			? OsxClipboard.SetText
+			: RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? LinuxClipboard.SetText : (_ => throw new NotSupportedException());
+	}
+}
+#endif
---
+++ RhuEngine/ClipBoard/LinuxClipboard_2.0.cs
@@ -0,0 +1,123 @@
+#if (NETSTANDARD2_0 || NETFRAMEWORK)
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+static class BashRunner
+{
+	public static string Run(string commandLine) {
+		StringBuilder errorBuilder = new();
+		StringBuilder outputBuilder = new();
+		var arguments = $"-c \"{commandLine}\"";
+		using Process process = new() {
+			StartInfo = new() {
+				FileName = "bash",
+				Arguments = arguments,
+				RedirectStandardOutput = true,
+				RedirectStandardError = true,
+				UseShellExecute = false,
+				CreateNoWindow = false,
+			}
+		};
+		process.Start();
+		process.OutputDataReceived += (_, args) => outputBuilder.AppendLine(args.Data);
+		process.BeginOutputReadLine();
+		process.ErrorDataReceived += (_, args) => errorBuilder.AppendLine(args.Data);
+		process.BeginErrorReadLine();
+		if (!process.DoubleWaitForExit()) {
+			var timeoutError = $@"Process timed out. Command line: bash {arguments}.
+Output: {outputBuilder}
+Error: {errorBuilder}";
+			throw new(timeoutError);
+		}
+		if (process.ExitCode == 0) {
+			return outputBuilder.ToString();
+		}
+
+		var error = $@"Could not execute process. Command line: bash {arguments}.
+Output: {outputBuilder}
+Error: {errorBuilder}";
+		throw new(error);
+	}
+
+	//To work around https://github.com/dotnet/runtime/issues/27128
+	static bool DoubleWaitForExit(this Process process) {
+		var result = process.WaitForExit(500);
+		if (result) {
+			process.WaitForExit();
+		}
+		return result;
+	}
+}
+
+
+static class LinuxClipboard
+{
+    static readonly bool _isWsl;
+
+    static LinuxClipboard()
+    {
+        _isWsl = Environment.GetEnvironmentVariable("WSL_DISTRO_NAME") != null;
+    }
+
+    public static Task SetTextAsync(string text, CancellationToken _)
+    {
+        SetText(text);
+
+        return Task.CompletedTask;
+    }
+
+    public static void SetText(string text)
+    {
+        var tempFileName = Path.GetTempFileName();
+        File.WriteAllText(tempFileName, text);
+        try
+        {
+            if (_isWsl)
+            {
+                BashRunner.Run($"cat {tempFileName} | clip.exe ");
+            }
+            else
+            {
+                BashRunner.Run($"cat {tempFileName} | xsel -i --clipboard ");
+            }
+        }
+        finally
+        {
+            File.Delete(tempFileName);
+        }
+    }
+
+    public static Task<string> GetTextAsync(CancellationToken _)
+    {
+        return Task.FromResult<string>(GetText());
+    }
+
+    public static string GetText()
+    {
+        var tempFileName = Path.GetTempFileName();
+        try
+        {
+            if (_isWsl)
+            {
+                BashRunner.Run($"powershell.exe Get-Clipboard  > {tempFileName}");
+            }
+            else
+            {
+                BashRunner.Run($"xsel -o --clipboard  > {tempFileName}");
+            }
+            return File.ReadAllText(tempFileName);
+        }
+        finally
+        {
+            File.Delete(tempFileName);
+        }
+    }
+}
+#endif
