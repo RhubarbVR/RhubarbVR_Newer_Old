1eadeb9ea936920914b61c7c177d9265e53ccc4d
KyuubiYoru <33806787+KyuubiYoru@users.noreply.github.com>
2022-05-01 20:29:09 +0200

    Add Wiggler/Wobbler components and SimplexNoise (#10)
    * Add Wiggler component
    
    * Cleanup and refactoring
    
    * Add Wobbler

---
+++ RhuEngine/Components/Transform/Wobbler2D.cs
@@ -0,0 +1,26 @@
+﻿using RhuEngine.WorldObjects;
+using RhuEngine.WorldObjects.ECS;
+using RNumerics;
+using RNumerics.Noise;
+
+namespace RhuEngine.Components.Transform
+{
+	[UpdateLevel(UpdateEnum.Movement)]
+	[Category(new string[] { "Transform" })]
+	public class Wobbler2D : Component
+	{
+		public Linker<Vector2f> driver;
+		public Sync<Vector2f> speed;
+		public Sync<Vector2f> magnitude;
+		public Sync<Vector2f> seed;
+		public Sync<Vector2f> offset;
+
+		public override void Step() {
+			if (driver.Linked) {
+				var time = (float)World.WorldTime;
+				var noiseValue = SimplexNoise.Generate2D(time, speed.Value, magnitude.Value, seed.Value);
+				driver.LinkedValue = offset.Value + noiseValue;
+			}
+		}
+	}
+}
---
+++ RhuEngine/Components/Transform/Wobbler3D.cs
@@ -0,0 +1,26 @@
+﻿using RhuEngine.WorldObjects;
+using RhuEngine.WorldObjects.ECS;
+using RNumerics;
+using RNumerics.Noise;
+
+namespace RhuEngine.Components.Transform
+{
+	[UpdateLevel(UpdateEnum.Movement)]
+	[Category(new string[] { "Transform" })]
+	public class Wobbler3D : Component
+	{
+		public Linker<Vector3f> driver;
+		public Sync<Vector3f> speed;
+		public Sync<Vector3f> magnitude;
+		public Sync<Vector3f> seed;
+		public Sync<Vector3f> offset;
+
+		public override void Step() {
+			if (driver.Linked) {
+				var time = (float)World.WorldTime;
+				var noiseValue = SimplexNoise.Generate3D(time, speed.Value, magnitude.Value, seed.Value);
+				driver.LinkedValue = offset.Value + noiseValue;
+			}
+		}
+	}
+}
