d88742cc516c12024a3d5c5a4e9bd4a5f5b49550
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2022-05-14 14:54:25 -0400

    Bug fixes for unix

--- RBullet/BulletPhsyicsLink.cs
+++ RBullet/BulletPhsyicsLink.cs
@@ -3,6 +3,9 @@ using System.Runtime.InteropServices;
 
 using RhuEngine.Physics;
 using BulletSharp;
+using System.IO;
+using System.Linq;
+using RhuEngine.Linker;
 
 namespace RBullet
 {  
@@ -12,7 +15,21 @@ namespace RBullet
 
 		public BulletPhsyicsLink() {
 			if (!Native.Load()) {
-				throw new Exception("Failed to load lib");
+				if (Environment.OSVersion.Platform == PlatformID.Unix) {
+					RLog.Info("Did not load lib at first");
+					var files = Directory.GetFiles("./../../", "*/*/linux-x64/native/libbulletc.so");
+					var filesarray = files.ToArray();
+					if (filesarray.Length == 0) {
+						throw new Exception("Failed to Find lib");
+					}
+					else {
+						File.Copy(filesarray[0], "./libbulletc.so");
+						RLog.Info("Did copy to try and get arround lib no loading");
+					}
+				}
+				else {
+					throw new Exception("Failed to load lib");
+				}
 			}
 		}
 
