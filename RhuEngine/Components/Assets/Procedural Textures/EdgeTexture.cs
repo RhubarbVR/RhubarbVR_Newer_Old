2fe6641a4d42e890eda3d8425117fa1c746e26b9
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-07-08 18:04:50 -0500

    Added edge texture

---
+++ RhuEngine/Components/Assets/Procedural Textures/EdgeTexture.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using RhuEngine.WorldObjects;
+
+using RNumerics;
+
+namespace RhuEngine.Components
+{
+	public class EdgeTexture : ProceduralTexture
+	{
+		[Default(1)]
+		[OnChanged(nameof(ComputeTexture))]
+		public readonly Sync<int> EdgeWidth;
+
+		[OnChanged(nameof(ComputeTexture))]
+		public readonly Sync<Colorf> BackgroundColor;
+
+		[OnChanged(nameof(ComputeTexture))]
+		public readonly Sync<Colorf> InnerColor;
+
+		public override void Generate() {
+
+			if (EdgeWidth.Value * 2 >= Size.Value.x || EdgeWidth.Value * 2 >= Size.Value.y) {
+				throw new Exception($"Edge width {EdgeWidth.Value} cannot be larger than texture size {Size.Value.x}x {Size.Value.y}y.");
+			}
+
+			var arr = new Colorf[Size.Value.x * Size.Value.y];
+
+			// Background
+			for (var i = 0; i < arr.Length; i++) {
+				arr[i] = BackgroundColor.Value;
+			}
+
+			// Inner
+			for (var i = EdgeWidth.Value; i < Size.Value.x - EdgeWidth.Value; i++) {
+				for (var j = EdgeWidth.Value; j < Size.Value.y - EdgeWidth.Value; j++) {
+					arr[(i * Size.Value.x) + j] = InnerColor.Value;
+				}
+			}
+
+			UpdateTexture(arr, Size.Value.x, Size.Value.y);
+		}
+	}
+}
