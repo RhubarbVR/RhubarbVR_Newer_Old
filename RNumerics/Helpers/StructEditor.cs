8b5c0480bfa617cbaab2843646be4c7c7ab1dc8c
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2023-01-08 22:41:22 -0500

    More struct editor

--- RNumerics/Helpers/StructEditor.cs
+++ RNumerics/Helpers/StructEditor.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+
+using Newtonsoft.Json.Linq;
 
 namespace RNumerics
 {
@@ -11,6 +14,25 @@ namespace RNumerics
 			return GetFielTypeHelper(targetType, fieldName);
 		}
 
+		private static (string field, int arrayPos)? GetArrayIndex(string fieldString) {
+			if (fieldString.Contains('[') & fieldString.Contains(']')) {
+				var start = fieldString.IndexOf('[');
+				var end = fieldString.IndexOf(']');
+				var field = fieldString.Substring(0, start);
+				try {
+					var arrayString = fieldString.Substring(start + 1, end - start - 1);
+					var arrayIndex = int.Parse(arrayString);
+					return (field, arrayIndex);
+				}
+				catch {
+					return (field, 0);
+				}
+			}
+			else {
+				return null;
+			}
+		}
+
 		private static Type GetFielTypeHelper(Type structType, string fieldName) {
 			var dotIndex = fieldName.IndexOf('.');
 			if (dotIndex >= 0) {
@@ -18,35 +40,90 @@ namespace RNumerics
 				var currentFieldName = fieldName.Substring(0, dotIndex);
 				var remainingFieldName = fieldName.Substring(dotIndex + 1);
 
-				// Get the field info for the current field
-				var fieldInfo = structType.GetField(currentFieldName);
+				var array = GetArrayIndex(currentFieldName);
+				if (array is null) {
+					// Get the field info for the current field
+					var fieldInfo = structType.GetField(currentFieldName);
 
-				// Check if the field exists
-				if (fieldInfo == null) {
-					throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
-				}
-				if (!fieldInfo.IsPublic) {
-					throw new Exception("Not Public Field");
-				}
+					// Check if the field exists
+					if (fieldInfo == null) {
+						throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
+					}
+					if (!fieldInfo.IsPublic) {
+						throw new Exception("Not Public Field");
+					}
 
-				// Recursively get the value of the next field in the hierarchy
-				return GetFielTypeHelper(fieldInfo.FieldType, remainingFieldName);
+					// Recursively get the value of the next field in the hierarchy
+					return GetFielTypeHelper(fieldInfo.FieldType, remainingFieldName);
+				}
+				else {
+					if (!string.IsNullOrEmpty(array.Value.field)) {
+						// Get the field info for the current field
+						var fieldInfo = structType.GetField(array.Value.field);
+						if (!fieldInfo.IsPublic) {
+							throw new Exception("Not Public Field");
+						}
+						// Check if the field exists
+						if (fieldInfo == null) {
+							throw new ArgumentException($"{array.Value.field} is not a field of type {structType.Name}");
+						}
+
+						return GetFielTypeHelper(fieldInfo.FieldType.GetElementType(), remainingFieldName);
+					}
+					else {
+						if (!structType.IsArray) {
+							throw new ArgumentException($"{array.Value.field} is not array");
+						}
+						// Return the value of the field
+						return GetFielTypeHelper(structType.GetElementType(), remainingFieldName);
+					}
+				}
 			}
 			else {
-				// This is the last field in the hierarchy, return the value
+				var array = GetArrayIndex(fieldName);
+				if (array is null) {
+					// Get the field info for the current field
+					var fieldInfo = structType.GetField(fieldName);
+					if (!fieldInfo.IsPublic) {
+						throw new Exception("Not Public Field");
+					}
+					// Check if the field exists
+					if (fieldInfo == null) {
+						throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
+					}
 
-				// Get the field info for the current field
-				var fieldInfo = structType.GetField(fieldName);
-				if (!fieldInfo.IsPublic) {
-					throw new Exception("Not Public Field");
+					// Return the value of the field
+					return fieldInfo.FieldType;
 				}
-				// Check if the field exists
-				if (fieldInfo == null) {
-					throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
-				}
+				else {
+					if (!string.IsNullOrEmpty(array.Value.field)) {
+						// Get the field info for the current field
+						var fieldInfo = structType.GetField(array.Value.field);
+						if (!fieldInfo.IsPublic) {
+							throw new Exception("Not Public Field");
+						}
 
-				// Return the value of the field
-				return fieldInfo.FieldType;
+						// Check if the field exists
+						if (fieldInfo == null) {
+							throw new ArgumentException($"{array.Value.field} is not a field of type {structType.Name}");
+						}
+
+						if (!fieldInfo.FieldType.IsArray) {
+							throw new ArgumentException($"{array.Value.field} is not array");
+						}
+
+						// Return the value of the field
+						return fieldInfo.FieldType.GetElementType();
+					}
+					else {
+						if (!structType.IsArray) {
+							throw new ArgumentException($"{array.Value.field} is not array");
+						}
+						// Return the value of the field
+						return structType.GetElementType();
+					}
+
+				}
 			}
 		}
 
@@ -63,39 +140,105 @@ namespace RNumerics
 				// Extract the current field name and remaining field name parts
 				var currentFieldName = fieldName.Substring(0, dotIndex);
 				var remainingFieldName = fieldName.Substring(dotIndex + 1);
+				var array = GetArrayIndex(fieldName);
+				if (array is null) {
+					// Get the field info for the current field
+					var fieldInfo = structType.GetField(currentFieldName);
 
-				// Get the field info for the current field
-				var fieldInfo = structType.GetField(currentFieldName);
+					// Check if the field exists
+					if (fieldInfo == null) {
+						throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
+					}
+					if (!fieldInfo.IsPublic) {
+						throw new Exception("Not Public Field");
+					}
 
-				// Check if the field exists
-				if (fieldInfo == null) {
-					throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
+					// Get the value of the current field
+					var fieldValue = fieldInfo.GetValue(targetStructBoxed);
+
+					// Recursively get the value of the next field in the hierarchy
+					return GetFieldValueHelper(fieldValue, fieldInfo.FieldType, remainingFieldName);
 				}
-				if (!fieldInfo.IsPublic) {
-					throw new Exception("Not Public Field");
+				else {
+					if (!string.IsNullOrEmpty(array.Value.field)) {
+						// Get the field info for the current field
+						var fieldInfo = structType.GetField(array.Value.field);
+
+						// Check if the field exists
+						if (fieldInfo == null) {
+							throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
+						}
+						if (!fieldInfo.IsPublic) {
+							throw new Exception("Not Public Field");
+						}
+
+						// Get the value of the current field
+						var fieldValue = fieldInfo.GetValue(targetStructBoxed);
+						if (fieldValue is IList list) {
+							var nest = list[array.Value.arrayPos];
+							return GetFieldValueHelper(nest, nest.GetType(), remainingFieldName);
+						}
+						else {
+							throw new ArgumentException($"{array.Value.field} is not a array type");
+						}
+					}
+					else {
+						if (targetStructBoxed is IList list) {
+							var nest = list[array.Value.arrayPos];
+							return GetFieldValueHelper(nest, nest.GetType(), remainingFieldName);
+						}
+						else {
+							throw new ArgumentException($"{array.Value.field} is not a array type");
+						}
+					}
 				}
-
-				// Get the value of the current field
-				var fieldValue = fieldInfo.GetValue(targetStructBoxed);
-
-				// Recursively get the value of the next field in the hierarchy
-				return GetFieldValueHelper(fieldValue, fieldInfo.FieldType, remainingFieldName);
 			}
 			else {
-				// This is the last field in the hierarchy, return the value
+				var array = GetArrayIndex(fieldName);
+				if (array is null) {
+					// Get the field info for the current field
+					var fieldInfo = structType.GetField(fieldName);
+					if (!fieldInfo.IsPublic) {
+						throw new Exception("Not Public Field");
+					}
+					// Check if the field exists
+					if (fieldInfo == null) {
+						throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
+					}
 
-				// Get the field info for the current field
-				var fieldInfo = structType.GetField(fieldName);
-				if (!fieldInfo.IsPublic) {
-					throw new Exception("Not Public Field");
+					// Return the value of the field
+					return fieldInfo.GetValue(targetStructBoxed);
 				}
-				// Check if the field exists
-				if (fieldInfo == null) {
-					throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
-				}
+				else {
+					if (!string.IsNullOrEmpty(array.Value.field)) {
+						// Get the field info for the current field
+						var fieldInfo = structType.GetField(array.Value.field);
 
-				// Return the value of the field
-				return fieldInfo.GetValue(targetStructBoxed);
+						// Check if the field exists
+						if (fieldInfo == null) {
+							throw new ArgumentException($"{array.Value.field} is not a field of type {structType.Name}");
+						}
+						if (!fieldInfo.IsPublic) {
+							throw new Exception("Not Public Field");
+						}
+
+						var startingArray = fieldInfo.GetValue(targetStructBoxed);
+						if (startingArray is IList list) {
+							return list[array.Value.arrayPos];
+						}
+						else {
+							throw new ArgumentException($"{array.Value.field} is not a array type");
+						}
+					}
+					else {
+						if (targetStructBoxed is IList list) {
+							return list[array.Value.arrayPos];
+						}
+						else {
+							throw new ArgumentException($"{array.Value.field} is not a array type");
+						}
+					}
+				}
 			}
 		}
 
@@ -114,51 +257,127 @@ namespace RNumerics
 		private static void SetFieldValueHelper(object targetStructBoxed, Type structType, string fieldName, object value) {
 			var dotIndex = fieldName.IndexOf('.');
 			if (dotIndex >= 0) {
+
 				// Extract the current field name and remaining field name parts
 				var currentFieldName = fieldName.Substring(0, dotIndex);
 				var remainingFieldName = fieldName.Substring(dotIndex + 1);
+				var array = GetArrayIndex(currentFieldName);
+				if (array is null) {
+					// Get the field info for the current field
+					var fieldInfo = structType.GetField(currentFieldName);
+					if (!fieldInfo.IsPublic) {
+						throw new Exception("Not Public Field");
+					}
+					// Check if the field exists
+					if (fieldInfo == null) {
+						throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
+					}
 
-				// Get the field info for the current field
-				var fieldInfo = structType.GetField(currentFieldName);
-				if (!fieldInfo.IsPublic) {
-					throw new Exception("Not Public Field");
+					// Get the value of the current field
+					var fieldValue = fieldInfo.GetValue(targetStructBoxed);
+
+					// Recursively set the value of the next field in the hierarchy
+					SetFieldValueHelper(fieldValue, fieldInfo.FieldType, remainingFieldName, value);
+
+					// Set the value of the current field
+					fieldInfo.SetValue(targetStructBoxed, fieldValue);
 				}
-				// Check if the field exists
-				if (fieldInfo == null) {
-					throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
+				else {
+					if (!string.IsNullOrEmpty(array.Value.field)) {
+						// Get the field info for the current field
+						var fieldInfo = structType.GetField(array.Value.field);
+						if (!fieldInfo.IsPublic) {
+							throw new Exception("Not Public Field");
+						}
+						// Check if the field exists
+						if (fieldInfo == null) {
+							throw new ArgumentException($"{array.Value.field} is not a field of type {structType.Name}");
+						}
+
+						// Get the value of the current field
+						var fieldValue = fieldInfo.GetValue(targetStructBoxed);
+
+						if (fieldValue is IList list) {
+							var data = list[array.Value.arrayPos];
+							SetFieldValueHelper(data, list[array.Value.arrayPos].GetType(), remainingFieldName, value);
+							list[array.Value.arrayPos] = data;
+						}
+						else {
+							throw new ArgumentException($"{array.Value.field} is not a array type");
+						}
+
+						// Set the value of the current field
+						fieldInfo.SetValue(targetStructBoxed, fieldValue);
+
+
+					}
+					else {
+						if (targetStructBoxed is IList list) {
+							var data = list[array.Value.arrayPos];
+							SetFieldValueHelper(data, list[array.Value.arrayPos].GetType(), remainingFieldName, value);
+							list[array.Value.arrayPos] = data;
+						}
+						else {
+							throw new ArgumentException($"{array.Value.field} is not a array type");
+						}
+					}
 				}
-
-				// Get the value of the current field
-				var fieldValue = fieldInfo.GetValue(targetStructBoxed);
-
-				// Recursively set the value of the next field in the hierarchy
-				SetFieldValueHelper(fieldValue, fieldInfo.FieldType, remainingFieldName, value);
-
-				// Set the value of the current field
-				fieldInfo.SetValue(targetStructBoxed, fieldValue);
 			}
 			else {
-				// This is the last field in the hierarchy, set the value
+				var array = GetArrayIndex(fieldName);
+				if (array is null) {
+					// Get the field info for the current field
+					var fieldInfo = structType.GetField(fieldName);
 
-				// Get the field info for the current field
-				var fieldInfo = structType.GetField(fieldName);
+					// Check if the field exists
+					if (fieldInfo == null) {
+						throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
+					}
+					if (!fieldInfo.IsPublic) {
+						throw new Exception("Not Public Field");
+					}
 
-				// Check if the field exists
-				if (fieldInfo == null) {
-					throw new ArgumentException($"{fieldName} is not a field of type {structType.Name}");
+					// Check if the value is of the correct type
+					if (!fieldInfo.FieldType.IsAssignableFrom(value.GetType())) {
+						// Attempt to convert the value to the correct type
+						value = Convert.ChangeType(value, fieldInfo.FieldType);
+					}
+
+					// Set the value of the field
+					fieldInfo.SetValue(targetStructBoxed, value);
 				}
-				if (!fieldInfo.IsPublic) {
-					throw new Exception("Not Public Field");
-				}
+				else {
+					if (!string.IsNullOrEmpty(array.Value.field)) {
+						// Get the field info for the current field
+						var fieldInfo = structType.GetField(array.Value.field);
 
-				// Check if the value is of the correct type
-				if (!fieldInfo.FieldType.IsAssignableFrom(value.GetType())) {
-					// Attempt to convert the value to the correct type
-					value = Convert.ChangeType(value, fieldInfo.FieldType);
-				}
+						// Check if the field exists
+						if (fieldInfo == null) {
+							throw new ArgumentException($"{array.Value.field} is not a field of type {structType.Name}");
+						}
+						if (!fieldInfo.IsPublic) {
+							throw new Exception("Not Public Field");
+						}
 
-				// Set the value of the field
-				fieldInfo.SetValue(targetStructBoxed, value);
+						var startingArray = fieldInfo.GetValue(targetStructBoxed);
+						if (startingArray is IList list) {
+							list[array.Value.arrayPos] = value;
+							// Set the value of the field
+							fieldInfo.SetValue(targetStructBoxed, startingArray);
+						}
+						else {
+							throw new ArgumentException($"{array.Value.field} is not a array type");
+						}
+					}
+					else {
+						if (targetStructBoxed is IList list) {
+							list[array.Value.arrayPos] = value;
+						}
+						else {
+							throw new ArgumentException($"{array.Value.field} is not a array type");
+						}
+					}
+				}
 			}
 		}
 
---
+++ RhuEngineTests/Helpers/StructEditorTests.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RNumerics;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNumerics.Tests
+{
+	public struct TestStruct
+	{
+		public int x;
+		public float y;
+	}
+	public struct TestArray
+	{
+		public int[] Value;
+	}
+	public struct TestNested
+	{
+		public TestArray testArray;
+		public TestStruct testStruct;
+	}
+	public struct TestNestedNested
+	{
+		public TestNested testStruct;
+		public int x;
+		public float y;
+	}
+	public struct TestOfTests
+	{
+		public TestStruct[] Value;
+	}
+	[TestClass()]
+	public class StructEditorTests
+	{
+		[TestMethod()]
+		public void TestNormalArrayStructs() {
+			var normal = new int[1] { 0 };
+			Assert.AreEqual(typeof(int), StructEditor.GetFielType(typeof(int[]), "[0]"));
+			StructEditor.SetFieldValue(ref normal, "[0]", 10);
+			Assert.AreEqual(10, normal[0]);
+			StructEditor.SetFieldValue(ref normal, "[0]", 130);
+			Assert.AreEqual(130, StructEditor.GetFieldValue(normal, "[0]"));
+		}
+
+		[TestMethod()]
+		public void TestOfTests() {
+			var normal = new TestOfTests { Value = new TestStruct[1] { new TestStruct { } } };
+			Assert.AreEqual(typeof(TestStruct), StructEditor.GetFielType(typeof(TestOfTests), "Value[0]"));
+			Assert.AreEqual(typeof(int), StructEditor.GetFielType(typeof(TestOfTests), "Value[0].x"));
+			StructEditor.SetFieldValue(ref normal, "Value[0].x", 10);
+			Assert.AreEqual(10, normal.Value[0].x);
+			StructEditor.SetFieldValue(ref normal, "Value[0].x", 130);
+			Assert.AreEqual(130, StructEditor.GetFieldValue(normal, "Value[0].x"));
+		}
+
+
+		[TestMethod()]
+		public void TestNestedArrayStructs() {
+			var normal = new TestArray { Value = new int[1] { 0 } };
+			Assert.AreEqual(typeof(int), StructEditor.GetFielType(typeof(TestArray), "Value[0]"));
+			StructEditor.SetFieldValue(ref normal, "Value[0]", 10);
+			Assert.AreEqual(10, normal.Value[0]);
+			StructEditor.SetFieldValue(ref normal, "Value[0]", 130);
+			Assert.AreEqual(130, StructEditor.GetFieldValue(normal, "Value[0]"));
+		}
+
+		[TestMethod()]
+		public void TestNormalStructs() {
+			var normal = new TestStruct { };
+			Assert.AreEqual(typeof(int), StructEditor.GetFielType(typeof(TestStruct), "x"));
+			Assert.AreEqual(typeof(float), StructEditor.GetFielType(typeof(TestStruct), "y"));
+			StructEditor.SetFieldValue(ref normal, "x", 10);
+			StructEditor.SetFieldValue(ref normal, "y", 10.1f);
+			Assert.AreEqual(10, normal.x);
+			Assert.AreEqual(10.1f, normal.y);
+			StructEditor.SetFieldValue(ref normal, "x", 130);
+			StructEditor.SetFieldValue(ref normal, "y", 130.1f);
+			Assert.AreEqual(130, StructEditor.GetFieldValue(normal, "x"));
+			Assert.AreEqual(130.1f, StructEditor.GetFieldValue(normal, "y"));
+		}
+
+		[TestMethod()]
+		public void TestNestedStructs() {
+			var normal = new TestNested { };
+			Assert.AreEqual(typeof(int), StructEditor.GetFielType(typeof(TestNested), "testStruct.x"));
+			Assert.AreEqual(typeof(float), StructEditor.GetFielType(typeof(TestNested), "testStruct.y"));
+			StructEditor.SetFieldValue(ref normal, "testStruct.x", 10);
+			StructEditor.SetFieldValue(ref normal, "testStruct.y", 10.1f);
+			Assert.AreEqual(10, normal.testStruct.x);
+			Assert.AreEqual(10.1f, normal.testStruct.y);
+			StructEditor.SetFieldValue(ref normal, "testStruct.x", 130);
+			StructEditor.SetFieldValue(ref normal, "testStruct.y", 130.1f);
+			Assert.AreEqual(130, StructEditor.GetFieldValue(normal, "testStruct.x"));
+			Assert.AreEqual(130.1f, StructEditor.GetFieldValue(normal, "testStruct.y"));
+
+		}
+		[TestMethod()]
+		public void TestNestedNestedStructs() {
+
+
+			var normal = new TestNestedNested { };
+			Assert.AreEqual(typeof(int), StructEditor.GetFielType(typeof(TestNestedNested), "testStruct.testStruct.x"));
+			Assert.AreEqual(typeof(float), StructEditor.GetFielType(typeof(TestNestedNested), "testStruct.testStruct.y"));
+			StructEditor.SetFieldValue(ref normal, "testStruct.testStruct.x", 10);
+			StructEditor.SetFieldValue(ref normal, "testStruct.testStruct.y", 10.1f);
+			Assert.AreEqual(10, normal.testStruct.testStruct.x);
+			Assert.AreEqual(10.1f, normal.testStruct.testStruct.y);
+			StructEditor.SetFieldValue(ref normal, "testStruct.testStruct.x", 130);
+			StructEditor.SetFieldValue(ref normal, "testStruct.testStruct.y", 130.1f);
+			Assert.AreEqual(130, StructEditor.GetFieldValue(normal, "testStruct.testStruct.x"));
+			Assert.AreEqual(130.1f, StructEditor.GetFieldValue(normal, "testStruct.testStruct.y"));
+
+		}
+	}
+}
