4f9c5c4940bdca69eaedf6ecf2315107ea6af6d0
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2023-03-29 20:47:08 -0400

    Update AssetResponse.cs

--- RhuEngine/AssetSystem/RequestStructs/AssetResponse.cs
+++ RhuEngine/AssetSystem/RequestStructs/AssetResponse.cs
@@ -19,20 +19,20 @@ namespace RhuEngine.AssetSystem.RequestStructs
 		public void DeSerlize(BinaryReader binaryReader) {
 			URL = binaryReader.ReadString();
 			MimeType = binaryReader.ReadString();
-			var length = binaryReader.ReadInt32();
-			binaryReader.ReadBytes(length);
 			CurrentPart = binaryReader.ReadInt32();
 			SizeOfPart = binaryReader.ReadInt32();
 			SizeOfData = binaryReader.ReadInt64();
+			var length = binaryReader.ReadInt32();
+			PartBytes = binaryReader.ReadBytes(length);
 		}
 
 		public void Serlize(BinaryWriter binaryWriter) {
 			binaryWriter.Write(URL);
 			binaryWriter.Write(MimeType);
-			binaryWriter.Write(PartBytes.Length);
-			binaryWriter.Write(PartBytes);
 			binaryWriter.Write(SizeOfPart);
 			binaryWriter.Write(SizeOfData);
+			binaryWriter.Write(PartBytes.Length);
+			binaryWriter.Write(PartBytes);
 		}
 	}
 }
