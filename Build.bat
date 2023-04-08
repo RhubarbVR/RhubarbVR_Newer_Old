d2a0237bcdc0cd0f494f8ad73d1cca92896367e5
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2023-04-08 03:02:29 -0400

    TouchUp

--- Build.bat
+++ Build.bat
@@ -5,15 +5,15 @@ set rhubarb_proj_path=./RhubarbEasyBuild/RhubarbEasyBuild.csproj
 :: check if dotnet is installed
 dotnet --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo ".NET runtime not found. Please download and install .NET."
+    echo ".NET SDK not found. Please download and install .NET 7 SDK."
     pause
     exit /b
 )
 
-:: check if .NET 6 and 7 runtimes are installed
+
 dotnet --list-runtimes | findstr /i "Microsoft.NETCore.App 7.\." >nul 2>&1
 if %errorlevel% neq 0 (
-    echo "Both .NET 6 runtime and 7 sdk are required. Please download and install them."
+    echo ".NET 7 sdk is required. Please download and install."
     pause
     exit /b
 )
